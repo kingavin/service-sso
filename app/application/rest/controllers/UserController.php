3dd787da36024067f1a66c989ebbc7c379fcdf04
Gavin Li <craftgavin@gmail.com>
2012-04-10 16:40:19 +0800

    added rest user controller to read user list

--- app/application/rest/controllers/UserController.php
+++ app/application/rest/controllers/UserController.php
@@ -1,38 +1,51 @@
 <?php
-class Admin_UserController extends Zend_Controller_Action 
+class Rest_UserController extends Zend_Rest_Controller 
 {
+	public function init()
+	{
+        $this->_helper->viewRenderer->setNoRender(true);
+        $this->_helper->layout()->disableLayout();
+	}
+	
 	public function indexAction()
 	{
 		$orgCode = $this->getRequest()->getParam('orgCode');
-		if(is_null($orgCode)) {
+		
+		$consumer = $this->getRequest()->getParam('consumer');
+		$timestamp = $this->getRequest()->getParam('timestamp');
+		$token = $this->getRequest()->getParam('token');
+		$sig = $this->getRequest()->getParam('sig');
+		
+//		$result = Class_SSO::validateSig($consumer, $timeStamp, $token, $sig, 15);
+		$result = true;
+		if($result == 'success') {
+			$co = App_Factory::_m('RemoteUser');
+			$co->setFields(array('loginName', 'userType'));
+			$co->addFilter('orgCode', $orgCode);
 			
+	        $result = array();
+			$co->sort('loginName', 1);
+			$data = $co->fetchAll(true);
+			$dataSize = $co->count();
+			
+			$result['data'] = $data;
+	        $this->_helper->json($result);
 		}
 	}
 	
-	public function createAction()
+	public function getAction()
 	{
 		
 	}
 	
-	public function editAction()
+	public function postAction()
 	{
-		$orgCode = $this->getRequest()->getParam('orgCode');
 		
-		require APP_PATH.'/admin/forms/User/Edit.php';
-		$form = new Form_User_Edit();
-		if($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getParams())) {
-			$ru = App_Factory::_m('RemoteUser');
-			$ruDoc = $ru->addFilter('loginName', $form->getValue('loginName'))
-				->fetchOne();
-			if(is_null($ruDoc)) {
-				$ruDoc = $ru->create();
-				$ruDoc->loginName = $form->getValue('loginName');
-				$ruDoc->orgCode = $orgCode;
-				$ruDoc->password = rand(111111, 999999);
-				$ruDoc->save();
-			}
-		}
-		$this->view->form = $form;
+	}
+	
+	public function putAction()
+	{
+		
 	}
 	
 	public function deleteAction()
