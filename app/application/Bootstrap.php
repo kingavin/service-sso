b626a62715e87baef48c5ffa72b69328af349187
Gavin Li <craftgavin@gmail.com>
2012-05-16 17:03:06 +0800

    ….use account.eo.test to control account

--- app/application/Bootstrap.php
+++ app/application/Bootstrap.php
@@ -25,8 +25,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $controller = Zend_Controller_Front::getInstance();
         $controller->setControllerDirectory(array(
             'default' => APP_PATH.'/default/controllers',
-        	'user' => APP_PATH.'/user/controllers',
-        	'admin' => APP_PATH.'/admin/controllers',
         	'rest' => APP_PATH.'/rest/controllers'
         ));
         
@@ -35,15 +33,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $layout = Zend_Layout::getMvcInstance();
         $layout->setLayout('template');
         
-        $csu = Class_Session_User::getInstance();
-        
-        $controller->registerPlugin(new App_Plugin_BackendSsoAuth(
-        	$csu,
-        	App_Plugin_BackendSsoAuth::SERVICE_SSO,
-        	Class_Server::API_KEY,
-        	array('admin', 'rest', 'user')
-        ));
-        //add action helper path
         Zend_Controller_Action_HelperBroker::addPath(APP_PATH.'/helpers', 'Helper');
         
         //add view helper path
@@ -55,18 +44,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	{
 		$controller = Zend_Controller_Front::getInstance();
 		$router = $controller->getRouter();
-		$csu = Class_Session_User::getInstance();
-		$userRoute = new Zend_Controller_Router_Route(
-			':userId/user/:controller/:action/*',
-			array(
-				'userId'    => $csu->getUserId(),
-				'module' => 'user',
-				'controller' => 'index',
-				'action'     => 'index'
-			),
-			array('userId' => '([a-z0-9]+)')
-		);
-		$router->addRoute('user', $userRoute);
 		$router->addRoute('rest', new Zend_Rest_Route($controller, array(), array('rest')));
 	}
 }
--- app/application/user/controllers/IndexController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class User_IndexController extends Zend_Controller_Action 
-{
-	public function indexAction()
-	{
-		
-	}
-}
